e9041de93d39e503798527c6f2cbe240c0332194
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-08 21:56:55 +0530

    Add files via upload

---
+++ FindDuplicateInt.java
@@ -0,0 +1,13 @@
+class Array12{
+    public static void main(String[] args){
+        int[] arr={2,3,4,2,4,5,6,5};
+        System.out.println("Duplicate Elements : ");
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
