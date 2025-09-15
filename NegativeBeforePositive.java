dd18f743230d6e48ef122730806ae2bdf7c33c71
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-15 11:45:19 +0530

    Add files via upload

---
+++ NegativeBeforePositive.java
@@ -0,0 +1,20 @@
+class Array48{
+    public static void main(String[] args){
+        int[] arr={-4,-3,-2,4,3,-1,7};
+        int[] newarr=new int[arr.length];
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                newarr[index++]=arr[i];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                newarr[index++]=arr[i];
+            }
+        }
+        for(int i:newarr){
+            System.out.print(i+" ");
+        }
+    }
+}
