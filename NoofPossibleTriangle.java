6055f9f597f684f2f2b7da1c39b19840d61f6b1c
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-15 11:45:52 +0530

    Add files via upload

---
+++ NoofPossibleTriangle.java
@@ -0,0 +1,30 @@
+class Array44{
+    public static void main(String[] args){
+          int[] arr = {4, 6, 3, 7};
+          int n=arr.length;
+          for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+          }
+          int count=0;
+         for(int i=arr.length-1;i>=1;i--){
+            int left=0;
+            int right=i-1;
+            while(left<right){
+                if(arr[left]+arr[right]>arr[i]){
+                    count+=(right-left);
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+         }
+         System.out.println(count);
+    }
+}
